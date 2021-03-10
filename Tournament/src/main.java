13a7998f4ed86b6bca5bebb615739479ddccabd2
Deekahy <43988231+NoxNoscope@users.noreply.github.com>
2021-03-10 14:27:35 +0100

    asdasd
    asdasd

---
+++ Tournament/src/main.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class main extends PApplet {
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("main");
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+    	size(500,500);
+        
+    }
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+        
+    }
+
+    // identical use to draw in Prcessing IDE
+    public void draw(){
+
+    }
+}
