a70b324507446bb2ca6bc47afeb3ba6d86f146a4
Deekahy <43988231+NoxNoscope@users.noreply.github.com>
2021-03-20 20:41:23 +0100

    aarrgghhh
    threw out like 4 new projects, and test projects becazuse it just didnt work. endeed up deleteing everything and jsut rerequestiing the unmodified github version.
    
    but it seems to all work now...i wanna cry it has been many many hours

--- Tournament/src/process/main.java
+++ Tournament/src/process/main.java
@@ -1,27 +1,29 @@
+package process;
 import processing.core.PApplet;
 
 public class main extends PApplet {
 
     // The argument passed to main must match the class name
     public static void main(String[] args) {
-        PApplet.main("main");
+        PApplet.main("process.main");
     }
 
     // method used only for setting the size of the window
     public void settings(){
-    	size(500,500);
+    	size(500,5000);
         
     }
 
+
     // identical use to setup in Processing IDE except for size()
     public void setup(){
-        
+        background(0);
+        stroke(255);
+        strokeWeight(10);
     }
 
     // identical use to draw in Prcessing IDE
     public void draw(){
-    	
-    	
-    	
+        line(0, 0, 500, 500);
     }
 }
--- Tournament/src/process/network.java
+++ Tournament/src/process/network.java
@@ -1,3 +1,4 @@
+package process;
 
 public class network {
 	
