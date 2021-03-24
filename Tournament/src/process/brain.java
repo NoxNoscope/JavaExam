bf067684a728ab8c990d8358a7d7c201480ad217
JEgelund <jonaskristensen112@hotmail.com>
2021-03-24 14:01:43 +0100

    ah yes

--- Tournament/src/process/brain.java
+++ Tournament/src/process/brain.java
@@ -1,5 +1,32 @@
 package process;
+import processing.core.PApplet;
 
-public class brain {
+import process.client;
 
+public class brain extends PApplet {
+	
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("process.brain");
+        
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+    	size(500,500);
+    	//diegoSettings();
+    }
+
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+        strokeWeight(5);
+    }
+
+    // identical use to draw in Processing IDE
+    public void draw(){
+        
+    }
+    
 }
