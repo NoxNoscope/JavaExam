55509fbe31611c8028a847323bf1bb312e7a70e5
zaltyy <72140998+zaltyy@users.noreply.github.com>
2021-04-14 13:28:48 +0200

    i want to be ninja
    processing to eclipse

--- Tournament/src/process/Gui.java
+++ Tournament/src/process/Gui.java
@@ -12,27 +12,34 @@ public class Gui extends PApplet {
         
     }
 
-    // method used only for setting the size of the window
-    public void settings(){
-    	size(500,500);
-    	//diegoSettings();
-    }
+
+public void settings() {
+  size(500, 800);
+}
+
+// identical use to setup in Processing IDE except for size()
+public void setup() {
+  gen(8);
+}
+
+// identical use to draw in Prcessing IDE
+public void draw() {
+  
+  
+  
+}
+
+void gen(int x){
+        for (int i = 0; i < x; i++) {
+        rect(30, (i)*60, 55, 55, 7);
+        
+}
 
 
-    // identical use to setup in Processing IDE except for size()
-    public void setup(){
-        strokeWeight(5);
-    }
-
-    // identical use to draw in Prcessing IDE
-    public void draw(){
-        line(0, 0, 500, 500);
-    }
+}
     
     
-    private void diegoSettings() {
-    	client startClient = new client("127.0.0.1");
-    	startClient.startClient();
-    }
+    
+    
     
 }
