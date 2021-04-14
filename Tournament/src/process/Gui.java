2a346066fb27682cee82bfd617faf005958b7587
zaltyy <72140998+zaltyy@users.noreply.github.com>
2021-04-14 13:48:35 +0200

    grovt layout

--- Tournament/src/process/Gui.java
+++ Tournament/src/process/Gui.java
@@ -1,45 +1,59 @@
 package process;
+
 import processing.core.PApplet;
 
 import process.client;
 
 public class Gui extends PApplet {
-	
 
-    // The argument passed to main must match the class name
-    public static void main(String[] args) {
-        PApplet.main("process.Gui");
-        
-    }
+	// The argument passed to main must match the class name
+	public static void main(String[] args) {
+		PApplet.main("process.Gui");
 
+	}
 
-public void settings() {
-  size(500, 800);
-}
+	public void settings() {
+		size(500, 800);
+	}
 
 // identical use to setup in Processing IDE except for size()
-public void setup() {
-  gen(8);
-}
+	public void setup() {
+		gen(8);
+		gen2(4);
+		gen3(2);
+		gen4(1);
+	}
 
 // identical use to draw in Prcessing IDE
-public void draw() {
-  
-  
-  
-}
+	public void draw() {
 
-void gen(int x){
-        for (int i = 0; i < x; i++) {
-        rect(30, (i)*60, 55, 55, 7);
-        
-}
+	}
 
-
+	void gen(int x) {
+		for (int i = 0; i < x; i++) {
+			rect(30, (i) * 60, 55, 55, 7);
+		}
+		
+	}
+		
+	void gen2(int x) {
+		for (int i = 0; i < x; i++) {
+			rect(30+55+55, (i) * 60, 55, 55, 7);
+		}
+	}
+	
+	void gen3(int x) {
+		for (int i = 0; i < x; i++) {
+			rect(30+220, (i) * 60, 55, 55, 7);
+		}
+	}
+	void gen4(int x) {
+		for (int i = 0; i < x; i++) {
+			rect(30+275+55, (i) * 60, 55, 55, 7);
+		}
+	}
+	
 }
-    
-    
-    
-    
-    
-}
+	
+	
+	
